5fb91f574c6429f8f0259cb9a4bd0f56acf97be8
Tulio <tulioruzo29@gmail.com>
2022-10-07 14:42:30 -0400

    basic layout added

---
+++ src/containers/Navbar.tsx
@@ -0,0 +1,9 @@
+type Props = {};
+
+export default function Navbar({}: Props) {
+   return (
+      <div className="mx-auto w-full sm:w-4/5 xl:w-2/3 bg-gray-700 h-16 text-white sticky top-0">
+         Navbar
+      </div>
+   );
+}
