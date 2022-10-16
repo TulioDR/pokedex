329f9bdb5976727e4ec0c2908499a3194ec73a0a
Tulio <tulioruzo29@gmail.com>
2022-10-16 13:10:49 -0400

    tap animation added to shuffle button

--- src/components/ShuffleBtn.tsx
+++ src/components/ShuffleBtn.tsx
@@ -1,4 +1,5 @@
 import usePokemonsContext from "../context/PokemonsContext";
+import { motion } from "framer-motion";
 
 type Props = {};
 
@@ -6,12 +7,13 @@ export default function ShuffleBtn({}: Props) {
    const { getRandomPokemons } = usePokemonsContext();
 
    return (
-      <button
-         onClick={getRandomPokemons}
+      <motion.button
+         whileTap={{ scale: 0.9 }}
+         onTap={getRandomPokemons}
          className="w-full h-10 bg-secondary space-x-2 flex items-center justify-center text-white rounded-lg"
       >
          <span className="material-icons">shuffle</span>
          <span className="text-lg">Surprise Me!</span>
-      </button>
+      </motion.button>
    );
 }
