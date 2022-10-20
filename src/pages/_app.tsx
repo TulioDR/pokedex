dbaaac7b015615a0e86b2306b1a13cdd16afdb2e
Tulio <tulioruzo29@gmail.com>
2022-10-20 16:24:49 -0400

    background image added to body

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <PokemonsProvider>
          <Navbar />
          <AnimatePresence mode="wait">
-            <motion.div key={router.route} className="w-full bg-black">
+            <motion.div key={router.route} className="w-full">
                <Container>
                   <div className="w-full bg-gray-300 p-5 sm:p-7 min-h-screen">
                      <Component {...pageProps} />
